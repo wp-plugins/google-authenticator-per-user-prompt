7d3a8357ce4472b9c6678b200c0bd9d76353526f
iandunn <iandunn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 16:28:33 +0000

    Re-adjust indentenation.
    git-svn-id: https://plugins.svn.wordpress.org/google-authenticator-per-user-prompt/trunk@1172903 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/token-prompt.php
+++ views/token-prompt.php
@@ -4,23 +4,23 @@
 	}
 ?>
 
-	<?php if ( $error_message ) : ?>
-		<div id="login_error">
-			<?php echo wp_kses( $error_message, array( 'strong' => array() ) ); ?>
-		</div>
-	<?php endif; ?>
-	
-	<form action="<?php echo esc_url( $action_url ); ?>" method="post" autocomplete="off">
-		<input type="hidden" name="user_id"           value="<?php echo absint( $user->ID ); ?>" />
-		<input type="hidden" name="gapup_login_nonce" value="<?php echo esc_attr( $_REQUEST['gapup_login_nonce'] ) ?>" />
-		<input type="hidden" name="redirect_to"       value="<?php echo esc_attr( $redirect_to ) ?>" />
-	
-		<?php GoogleAuthenticator::$instance->loginform(); ?>
-	
-		<p class="submit">
-			<input type="submit" id="gapup_token_prompt" name="gapup_token_prompt" class="button button-primary button-large" value="<?php esc_attr_e( 'Log In' ); ?>" />
-		</p>
-	</form>
+<?php if ( $error_message ) : ?>
+	<div id="login_error">
+		<?php echo wp_kses( $error_message, array( 'strong' => array() ) ); ?>
+	</div>
+<?php endif; ?>
+
+<form action="<?php echo esc_url( $action_url ); ?>" method="post" autocomplete="off">
+	<input type="hidden" name="user_id"           value="<?php echo absint( $user->ID ); ?>" />
+	<input type="hidden" name="gapup_login_nonce" value="<?php echo esc_attr( $_REQUEST['gapup_login_nonce'] ) ?>" />
+	<input type="hidden" name="redirect_to"       value="<?php echo esc_attr( $redirect_to ) ?>" />
+
+	<?php GoogleAuthenticator::$instance->loginform(); ?>
+
+	<p class="submit">
+		<input type="submit" id="gapup_token_prompt" name="gapup_token_prompt" class="button button-primary button-large" value="<?php esc_attr_e( 'Log In' ); ?>" />
+	</p>
+</form>
 	
 <?php
 	if ( function_exists( 'login_footer' ) ) {
